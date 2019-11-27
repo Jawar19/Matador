2e6edfeffe3e779616f627fb5dbfa2bd89e763dd
Mikkel <Mikkel.a.j@gmail.com>
2019-11-27 17:19:29 +0100

    Field klasser

---
+++ src/matador/BuyableField.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package matador;
+
+/**
+ *
+ * @author Mikke
+ */
+abstract class BuyableField implements IField
+{
+
+    @Override
+    public void landOnField(Player P)
+    {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
--- src/matador/Main.java
+++ src/matador/Main.java
@@ -11,7 +11,6 @@ package matador;
  */
 public class Main
 {
-    
 
     /**
      * @param args the command line arguments
@@ -24,11 +23,10 @@ public class Main
         boolean isGameOver = false;
         short turnCount = 0;
         short eyeCount;
-        
-        
+
         Controller MatadorController = new Controller();
-        
-        do 
+
+        do
         {
             switch (preGame)
             {
@@ -40,40 +38,36 @@ public class Main
                     {
                         System.out.println("at least 2 players are needed to start the game");
                         break;
-                    }
-                    else
+                    } else
                     {
                         startGame = true;
                         break;
                     }
-            }                   
-        }
-        while(MatadorController.playerList.size() < 6 || !startGame);
-    
-    do{
-        
-        switch (gameOrder)
+            }
+        } while (MatadorController.playerList.size() < 6 || !startGame);
+
+        do
         {
-            case "nextPlayer":
-                System.out.println("Next player is: " + MatadorController.playerList.get(turnCount).GetName());
-                gameOrder = "rollDice";
-                break;
-            case "rollDice":
-                if (MatadorController.rollDice())
-                {
-                    gameOrder = "moveJail";
-                }
-                else
-                {
-                    gameOrder = "movePlayer";
-                }
-            case "movePlayer":
-                MatadorController.playerList.get(turnCount).movePlayer(MatadorController.getDiceValue());
-        }
-        
-    } while (!isGameOver);
+
+            switch (gameOrder)
+            {
+                case "nextPlayer":
+                    System.out.println("Next player is: " + MatadorController.playerList.get(turnCount).GetName());
+                    gameOrder = "rollDice";
+                    break;
+                case "rollDice":
+                    if (MatadorController.rollDice())
+                    {
+                        gameOrder = "moveJail";
+                    } else
+                    {
+                        gameOrder = "movePlayer";
+                    }
+                case "movePlayer":
+                    MatadorController.playerList.get(turnCount).movePlayer(MatadorController.getDiceValue());
+            }
+
+        } while (!isGameOver);
     }
-    
-    
-    
+
 }
